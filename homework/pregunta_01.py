8165c87cbe7a882eb8b4f83543e787064f2a8310
yezapataa-create <yezapataa@unal.edu.co>
2026-06-16 22:34:07 -0500

    Intento 1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -3,7 +3,9 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
 
 # pylint: disable=import-outside-toplevel
-
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def pregunta_01():
     """
@@ -16,3 +18,82 @@ def pregunta_01():
     El gráfico debe salvarse al archivo `files/plots/news.png`.
 
     """
+    df = pd.read_csv('files/input/news.csv', index_col=0)
+
+    plt.Figure()
+
+    colors = {
+        'Television': 'dimgray',
+        'Newspaper': 'grey',
+        'Internet': 'tab:blue',
+        'Radio': 'lightgrey',
+    }
+
+    zorder = {
+        'Television': 1,
+        'Newspaper': 1,
+        'Internet': 2,
+        'Radio': 1,
+    }
+
+    linewidths = {
+        'Television': 2,
+        'Newspaper': 2,
+        'Internet': 4,
+        'Radio': 2,
+    }
+
+    for col in df.columns:
+        plt.plot(
+            df[col],
+            color=colors[col],
+            label=col,
+            zorder=zorder[col],
+            linewidth=linewidths[col]
+            )
+
+    plt.title('How people get their news', fontsize=16)
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+    plt.gca().spines['left'].set_visible(False)
+    plt.gca().axes.get_yaxis().set_visible(False)
+
+    for col in df.columns:
+        first_year = df.index[0]
+        plt.scatter(
+            x=first_year,
+            y=df[col][first_year],
+            color=colors[col],
+            zorder=zorder[col],
+        )
+
+
+        plt.text(
+            first_year - 0.2,
+            df[col][first_year],
+            col + '' + str(df[col][first_year]) + "%",
+            ha='right',
+            va='center',
+            color=colors[col],
+        ) 
+
+        last_year = df.index[-1]
+        plt.scatter(
+            x=last_year,
+            y=df[col][last_year],
+            color=colors[col],
+            zorder=zorder[col],
+        )
+
+        plt.text(
+            last_year + 0.2,
+            df[col][last_year],
+            col + '' + str(df[col][last_year]) + "%",
+            ha='left',
+            va='center',
+            color=colors[col],
+        )
+    
+    plt.tight_layout()
+    os.makedirs('files/plots', exist_ok=True)
+    plt.savefig('files/plots/news.png')
